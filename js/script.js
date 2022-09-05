b7fa2216a7a9749ac9a239fa905db37bf5f2ccee
Jeffrey <77903214+jgrospe92@users.noreply.github.com>
2022-09-05 15:57:24 -0400

    Added Ajax function & fixed css
    working live search with ajax
    Fix css on the search bar and suggestions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,43 @@
-var userInput = document.getElementById('query').value;
-console.log(userInput);
+const query = "https://suggestqueries.google.com/complete/search?client=chrome&q=";
+const searchQuery = "https://www.google.ca/search?q="
 
 
-function getQuery(){
+$("#searchQuery").keydown(function(event){
+    if(event.code === 'Enter'){
+        searchWithVal($("#searchQuery").val());
+    }
+})
 
+
+$("#searchQuery").on("input", function() {
+    const value = $(this).val();
+    if(value==="") {
+        $(".suggestions").empty();
+        $("#search").css("border-radius","3%/50%");
+        return;
+    } 
+    $.ajax({
+        url:query+''+value, 
+        dataType: 'jsonp',
+        success: function(result) {
+            $(".suggestions").empty();
+            $("#search").css({"border-bottom-left-radius":"0","border-bottom-right-radius":"0"});
+            $.each(result[1], function(key, val) {
+                const newDiv = document.createElement("div");
+                newDiv.className="suggestionDiv";
+                const newSpan = document.createElement("span");
+                newSpan.textContent = val;
+                newDiv.appendChild(newSpan);
+                newDiv.addEventListener("click", function() {
+                    searchWithVal(this.children[0].textContent);
+                });
+                $(".suggestions").append(newDiv);
+            })
+        }
+    })
+})
+
+function searchWithVal(val) {
+    const newVal = val.replace(' ','+');
+    window.open(searchQuery+''+newVal,'_blank').focus();
 }
